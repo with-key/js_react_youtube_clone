f9bbc88638032964455441fbdffed117f5a8e62b
withWws <66957178+withWws@users.noreply.github.com>
2020-11-06 00:51:11 +0900

    list design

--- src/components/videoItem/VideoItem.jsx
+++ src/components/videoItem/VideoItem.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function VideoItem({ video }) {
+  //   console.log(video);
   // 객체 비구조화
   const {
-    snippet: { title },
+    snippet: {
+      title,
+      thumbnails: {
+        default: { url },
+      },
+    },
   } = video;
 
-  return <h1>{title}</h1>;
+  return (
+    <>
+      <ListGrid>
+        <h4>{title}</h4>
+        <img src={url} alt="thumbnails" />
+      </ListGrid>
+    </>
+  );
 }
+
+const ListGrid = styled.div`
+  width: 400px;
+  border: 1px solid crimson;
+`;
--- src/components/videoList/VideoLists.jsx
+++ src/components/videoList/VideoLists.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import VideoItem from "../videoItem/VideoItem";
+import styled from "styled-components";
 
 const VideoLists = (props) => {
   return (
-    <ul>
+    <Container>
       {props.videos.map((video) => (
         <VideoItem key={video.id} video={video} />
       ))}
-    </ul>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  justify-content: center;
+  display: flex;
+  flex-wrap: wrap;
+`;
 
 export default VideoLists;
